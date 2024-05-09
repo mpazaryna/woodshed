f09c7b35a37a98f069a78d62d6204a95694a206a
Matthew Pazaryna <mpazbot@gmail.com>
2024-05-09 15:04:20 +0000

    added limerick option

--- ai/apps/chat-haiku/st_haiku.py
+++ ai/apps/chat-haiku/st_haiku.py
@@ -15,13 +15,28 @@ def generate_haiku(topic):
     output = chain.invoke({"text": topic})
     st.write(output)
 
+def generate_limerick(topic):
+
+    system = "You are a poet able to writh the most beautiful limerick on any subject."
+    human = "{text}"
+    prompt = ChatPromptTemplate.from_messages([("system", system), ("human", human)])
+
+    chain = prompt | chat | StrOutputParser()
+    output = chain.invoke({"text": topic})
+    st.write(output)
+
+
 
 def main():
-    st.title("Haiku Generator")
+    st.title("Poetry Bot")
     topic = st.text_input("Enter a topic for your haiku:")
 
     if st.button("Generate Haiku"):
         generate_haiku(topic)
+    
+    if st.button("Generate Limerick"):
+        generate_limerick(topic)
+
 
 
 if __name__ == "__main__":
