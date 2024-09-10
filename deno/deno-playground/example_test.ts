9ebd187ffd9fb1de01a523a5fdb1303b4a7058a6
mpazaryna <mpazbot@gmail.com>
2024-09-10 15:12:20 -0400

    chore: remove cruft

--- deno/deno-playground/example_test.ts
+++
@@ -1,6 +0,0 @@
-Deno.test("example test", () => {
-    const result = 1 + 1;
-    if (result !== 2) {
-        throw new Error("Test failed: 1 + 1 should equal 2");
-    }
-});
--- deno/deno-playground/hello.ts
+++
@@ -1,4 +0,0 @@
-import { getName } from "./name.ts"; // Import the getName function
-
-const name = getName(); // Get the user's name
-console.log(`Hello, ${name}!`); // Respond with the user's name
